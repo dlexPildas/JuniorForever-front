f4ed0c6a7b087e729e25a4d900288f47ba05affa
Daniel Lima de Oliveira <oliveiradaniel11@hotmail.com>
2020-07-07 13:47:52 -0300

    correct environoment var

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { getToken } from './auth';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
+    baseURL: 'http://juniorforever.azurewebsites.net/'
 })
 
 api.interceptors.request.use(async config => {
